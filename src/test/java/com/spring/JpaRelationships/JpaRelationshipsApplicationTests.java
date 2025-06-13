e0223e2b75253d93378528b9a0321077fe64f47b
marinescu97 <marinescumonica97@yahoo.com>
2025-06-13 12:59:29 +0300

    Added Homework 24

---
+++ src/test/java/com/spring/JpaRelationships/JpaRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.JpaRelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
